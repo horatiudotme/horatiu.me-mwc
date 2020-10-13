492b7f926a7daae8f3ba514a6e93f747ba715113
helloworldwriter <writer@helloworldwriter.com>
2020-10-13 16:39:13 +0300

    Add tabs; add intro; improve styles

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -1,6 +1,9 @@
 import '@material/mwc-top-app-bar-fixed';
 import '@material/mwc-icon-button';
 
+import '@material/mwc-tab-bar';
+import '@material/mwc-tab';
+
 import '@material/mwc-list';
 import '@material/mwc-list/mwc-list-item';
 
