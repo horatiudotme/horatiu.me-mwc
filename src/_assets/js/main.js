e23970093ad8f8b9c853a18721f5816e54504ae6
techaurelian <dev@techaurelian.com>
2020-10-14 11:59:56 +0300

    Implement tab bar switching

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -9,3 +9,10 @@ import '@material/mwc-list/mwc-list-item';
 
 
 import '@material/mwc-button';
+
+const tabBarViews = document.querySelectorAll('.hme-tab-bar-view');
+
+const mainTabBar = document.getElementById('main-tab-bar');
+mainTabBar.addEventListener('MDCTabBar:activated', (e) => {
+  tabBarViews.forEach((view, index) => view.hidden = index != e.detail.index);
+});
